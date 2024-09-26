32c61214a45844bf37bab9c43c9da5d41fb25c3d
Yasith_99 <yasithsarada79@gmail.com>
2024-09-27 01:49:35 +0530

    third commit

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,17 @@
+// module.exports = {
+//     plugins: [
+//       require('postcss-import'),
+//       require('tailwindcss/nesting')(require('postcss-nesting')),
+//       require('autoprefixer'),
+//       require('tailwindcss'),
+//     ]
+//   }
+
 module.exports = {
-    plugins: [
-      require('postcss-import'),
-      require('tailwindcss/nesting')(require('postcss-nesting')),
-      require('autoprefixer'),
-      require('tailwindcss'),
-    ]
+    plugins: {
+      'tailwindcss/nesting': {},  // Add this line to support nested CSS
+      tailwindcss: {},
+      autoprefixer: {},
+    },
   }
+  
